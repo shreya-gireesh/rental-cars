66f53ac40c0db9aac67dc31d17e8293ae0587176
shreya-gireesh <shreya.gs01@gmail.com>
2024-04-24 15:49:08 +0530

    Your commit message here

--- RentalCar/settings.py
+++ RentalCar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xdcmbys=mxpdzg)24%jm^88uvinyg$z+)fx$%o+d83obz%=^e_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
